bff125690cff641761b849f435b165cdcb8b7422
Aliyor <aliyor2020@gmail.com>
2021-05-05 10:31:00 +0500

    substr

--- script.js
+++ script.js
@@ -76,7 +76,58 @@
 // console.log(isChacked && isClose);
 // console.log(isChacked || !isClose);
 
-const numberOfFilms = prompt('Siz nechita kino korgansiz?', '');
+// if (5 == 6) {
+//     console.log('ok');
+// } else {
+//     console.log('error');
+// }
+
+
+// if (num < 49) {
+//     console.log('error');
+// } else if (num > 100) {
+//     console.log('kop');
+// } else {
+//     console.log('ok');
+// }
+// (num === 50) ? console.log('ok') : console.log('error');
+
+// switch (num) {
+//     case 49:
+//         console.log('notogri');
+//         break;
+//     case 100: 
+//         console.log('notogri');
+//         break;
+//     case 52:
+//         console.log('togri');
+//         break;
+//     default: 
+//         console.log('aniqlanmadi');
+//         break;
+// }
+
+// let num = 50;
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num <= 55);
+// for (let i = 1; i < 10; i++) {
+//     if (i === 4) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+const numberOfFilms = +prompt('Siz nechita kino korgansiz?', '');
+// if (numberOfFilms == 100) {
+//     const numberOfFilms = +prompt('Siz nechita kino korgansiz?', '');
+// } 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -84,10 +135,63 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Oxirigi korgan filmingiz?', ''),
-      b = prompt('nechi baxo berasiz?', ''),
-      c = prompt('Oxirigi korgan filmingiz?', ''),
-      d = prompt('nechi baxo berasiz?', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+// const a = prompt('Oxirigi korgan filmingiz?', ''),
+//       b = prompt('nechi baxo berasiz?', ''),
+//       c = prompt('Oxirigi korgan filmingiz?', ''),
+//       d = prompt('nechi baxo berasiz?', '');
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
+// switch (personalMovieDB.count) {
+//     case 10:
+//         console.log('kam');
+//         break;
+//     case 30:
+//         console.log('yaxshi');
+//         break;
+//     case 50:
+//         console.log('kop');
+//         break;
+//     default:
+//         console.log('bilmadim');
+//         break;
+// }
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Oxirigi korgan filmingiz?', ''),
+//           b = prompt('nechi baxo berasiz?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     }   else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+// if (personalMovieDB.count < 10) {
+//     console.log('kam');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log('yaxshi');
+// } else if (personalMovieDB.count >= 30) {
+//     console.log('kop');
+// } else {
+//     console.log('xato');
+// }
+
+// console.log(personalMovieDB);
+// let num = 10;
+// function showFirstMessage(alex) {
+//     console.log(alex);
+//     num = 20;
+// }
+
+// showFirstMessage('Hellow World!');
+// console.log(num);
+// const str = 'Test';
+// const arr = [1, 2, 4];
+// console.log(arr.length);
+// console.log(str.toUpperCase());
+// console.log(str);
+const logg = 'Hello World';
+console.log(logg.substr(6, 5));
